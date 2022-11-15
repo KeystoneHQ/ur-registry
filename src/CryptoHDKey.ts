6d84c23fb26ecec5953218aec58e37d7116358a1
soralit <soralitria@gmail.com>
2022-11-15 09:45:14 +0800

    fix: confusing dependency

--- src/CryptoHDKey.ts
+++ src/CryptoHDKey.ts
@@ -1,5 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 import { encode } from 'bs58check';
 import { CryptoCoinInfo } from './CryptoCoinInfo';
 import { CryptoKeypath } from './CryptoKeypath';
